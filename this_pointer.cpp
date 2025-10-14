8fd2dad27de3b5545872f004522036dc5ebd1aaa
Shaik-Anwar132 <apsarshaik132@gmail.com>
2025-10-14 22:14:44 +0530

    Create this_pointer.cpp

---
+++ this_pointer.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+using namespace std; 
+class Employee { 
+private: 
+    int id; 
+    string name; 
+public: 
+    // Setter using 'this' pointer to resolve name conflict 
+    void setDetails(int id, string name) { 
+        this->id = id;      
+        this->name = name; 
+  } 
+   
+    void display() { 
+        cout << "Employee ID: " << this->id << endl; 
+        cout << "Employee Name: " << this->name << endl; 
+    } 
+}; 
+int main() { 
+    Employee emp1; 
+    emp1.setDetails(101, "Laxman"); 
+    emp1.display(); 
+    return 0; 
+}
